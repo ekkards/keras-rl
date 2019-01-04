70f0f827c295a4172e4d924deee3f33aa8fc6306
ekkards <es@schnedermann.de>
2019-01-04 13:03:55 +0100

    adapting for better understanding

--- examples/dqn_atari.py
+++ examples/dqn_atari.py
@@ -19,7 +19,17 @@ from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
 INPUT_SHAPE = (84, 84)
 WINDOW_LENGTH = 4
+parser = argparse.ArgumentParser()
+parser.add_argument('--mode', choices=['train', 'test'], default='train')
+parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v4')
+parser.add_argument('--weights', type=str, default=None)
+args = parser.parse_args()
 
+# Get the environment and extract the number of actions.
+env = gym.make(args.env_name)
+np.random.seed(123)
+env.seed(123)
+nb_actions = env.action_space.n
 
 class AtariProcessor(Processor):
     def process_observation(self, observation):
@@ -39,18 +49,6 @@ class AtariProcessor(Processor):
 
     def process_reward(self, reward):
         return np.clip(reward, -1., 1.)
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--mode', choices=['train', 'test'], default='train')
-parser.add_argument('--env-name', type=str, default='BreakoutDeterministic-v4')
-parser.add_argument('--weights', type=str, default=None)
-args = parser.parse_args()
-
-# Get the environment and extract the number of actions.
-env = gym.make(args.env_name)
-np.random.seed(123)
-env.seed(123)
-nb_actions = env.action_space.n
 
 # Next, we build our model. We use the same model that was described by Mnih et al. (2015).
 input_shape = (WINDOW_LENGTH,) + INPUT_SHAPE
